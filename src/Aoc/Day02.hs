6dc49df656b044bb5c01305904a38cc2bfe256d9
David Tchepak <dave@davesquared.net>
2022-12-05 07:40:51 +1100

    Day 2 part 1

--- src/Aoc/Day02.hs
+++ src/Aoc/Day02.hs
@@ -1,11 +1,66 @@
 module Aoc.Day02 (
+  Shape,
+  Result,
+  unsafeGameScore,
+  result,
+  sumOfScores,
   answers
 ) where
 
 readInput :: IO String
 readInput = readFile "data/day02.txt"
 
+data Shape = Rock | Paper | Scissors deriving (Show, Eq)
+
+data Result = Win | Draw | Loss deriving (Show, Eq)
+
+unsafeParseShape :: Char -> Shape
+unsafeParseShape c = case c of
+  'A' -> Rock
+  'B' -> Paper
+  'C' -> Scissors
+  'X' -> Rock
+  'Y' -> Paper
+  'Z' -> Scissors
+  _   -> error ("Unknown shape: " ++ [c])
+
+unsafeGameScore :: String -> Int
+unsafeGameScore (x : ' ' : y : []) = gameScore (unsafeParseShape x) (unsafeParseShape y)
+unsafeGameScore s = error ("Unknown game format: " ++ s)
+
+resultScore :: Result -> Int
+resultScore Win = 6
+resultScore Draw = 3
+resultScore Loss = 0
+
+shapeScore :: Shape -> Int
+shapeScore Rock = 1
+shapeScore Paper = 2
+shapeScore Scissors = 3
+
+-- | Result for player of second shape
+result :: Shape -> Shape -> Result
+result Rock Rock = Draw
+result Rock Paper = Win
+result Rock Scissors = Loss
+
+result Paper Rock = Loss
+result Paper Paper = Draw
+result Paper Scissors = Win
+
+result Scissors Rock = Win
+result Scissors Paper = Loss
+result Scissors Scissors = Draw
+
+-- | Score for player of second shape
+gameScore :: Shape -> Shape -> Int
+gameScore theirs ours = shapeScore ours + resultScore (result theirs ours)
+
+sumOfScores :: String -> Int
+sumOfScores = sum . fmap unsafeGameScore . lines
+
 answers :: IO ()
 answers = do
   input <- readInput
-  putStrLn input
+  putStrLn "Part 1"
+  print (sumOfScores input)
--- test/Aoc/Day02Spec.hs
+++ test/Aoc/Day02Spec.hs
@@ -3,8 +3,16 @@ module Aoc.Day02Spec (spec) where
 import Test.Hspec
 import Aoc.Day02
 
+
 spec :: Spec
 spec = do
   describe "example data" $ do
-    it "part 1: example input parsing" $
-      1 `shouldBe` 1
+    let example = "A Y\nB X\nC Z"
+    it "part 1: example A Y" $
+      unsafeGameScore "A Y" `shouldBe` 8
+    it "part 1: example B X" $
+      unsafeGameScore "B X" `shouldBe` 1
+    it "part 1: example C Z" $
+      unsafeGameScore "C Z" `shouldBe` 6
+    it "part 1: example input" $
+      sumOfScores example `shouldBe` 15
