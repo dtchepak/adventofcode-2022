00d416005fdd618006ce0e21e5d1728ba6635566
David Tchepak <dave@davesquared.net>
2022-12-06 22:38:01 +1100

    Day 3 part 1

---
+++ src/Aoc/Day03.hs
@@ -0,0 +1,40 @@
+module Aoc.Day03 (
+  compartments,
+  inBoth,
+  compartmentPriority,
+  part1,
+  answers
+) where
+
+import Data.Char (ord)
+import qualified Data.Maybe as Maybe
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+
+readInput :: IO T.Text
+readInput = T.readFile "data/day03.txt"
+
+compartments :: T.Text -> (T.Text, T.Text)
+compartments t = T.splitAt (T.length t `div` 2) t
+
+inBoth :: T.Text -> T.Text -> Maybe Char
+inBoth t = T.find (`T.elem` t)
+
+priority :: Char -> Int
+priority c
+  | c >= 'a' && c <= 'z' = ord c - ord 'a' + 1
+  | c >= 'A' && c <= 'Z' = ord c - ord 'A' + 27
+  | otherwise = error $ "unexpected char: " ++ show c
+
+compartmentPriority :: T.Text -> Maybe Int
+compartmentPriority = fmap priority . uncurry inBoth . compartments
+
+part1 :: T.Text -> Int
+part1 = sum . Maybe.mapMaybe compartmentPriority . T.lines
+
+answers :: IO ()
+answers = do
+  input <- readInput
+  putStrLn "Part 1:"
+  print (part1 input)
+  putStrLn "Part 2:"
---
+++ test/Aoc/Day03Spec.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Aoc.Day03Spec (spec) where
+
+import Aoc.Day03
+import Test.Hspec
+import Test.Hspec.QuickCheck
+import Test.QuickCheck
+import Data.Maybe
+import qualified Data.Text as T
+
+spec :: Spec
+spec = do
+  describe "example data" $ do
+    let exampleData = [
+            "vJrwpWtwJgWrhcsFMMfFFhFp",
+            "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+            "PmmdzqPrVvPwwTWBwg",
+            "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+            "ttgJtRGJQctTZtZT",
+            "CrZsJsPPZsGzwwsLwLmpwMDw"
+          ]
+    it "part 1: find common items" $
+      mapMaybe (uncurry inBoth . compartments) exampleData === "pLPvts"
+    it "part 1: compartment priorities" $
+      mapMaybe compartmentPriority exampleData === [16, 38, 42, 22,  20, 19]
+    it "part 1: example" $
+      part1 (T.unlines exampleData) === 157
