e8592818d8930126ab27114d93dba8d3db3a14f8
David Tchepak <dave@davesquared.net>
2022-12-08 12:46:38 +1100

    Day 4 parts 1 and 2

--- src/Aoc/Day04.hs
+++ src/Aoc/Day04.hs
@@ -1,13 +1,65 @@
-module Aoc.Day04 where
+module Aoc.Day04 (
+  pairFullyContains,
+  fullyContained,
+  anyOverlap,
+  answers
+) where
 
+import Data.Bifunctor (first)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
+import Data.Void
+import Text.Megaparsec hiding (State)
+import Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer as L
+
+type Parser = Parsec Void T.Text
+type PairAssignment = (Range Integer, Range Integer)
+
+data Range a = Range a a deriving (Show, Eq)
+
+pRange :: Parser (Range Integer)
+pRange = Range <$> L.decimal <*> (char '-' *> L.decimal)
+
+pRangePair :: Parser PairAssignment
+pRangePair = (,) <$> pRange <*> (char ',' *> pRange)
+
+pPairs :: Parser [PairAssignment]
+pPairs = pRangePair `sepBy` newline
 
 readInput :: IO T.Text
 readInput = T.readFile "data/day04.txt"
+
+parsePairs :: T.Text -> Either String [PairAssignment]
+parsePairs = first show . parse pPairs ""
+
+-- | True if first range fully contains the second
+contains :: Ord a => Range a -> Range a -> Bool
+contains (Range lower upper) (Range lower' upper') =
+  lower <= lower' && upper >= upper'
+
+overlaps :: Ord a => Range a -> Range a -> Bool
+overlaps (Range lower upper) (Range lower' upper') =
+  not (upper < lower' || lower > upper')
+
+
+pairFullyContains :: PairAssignment -> Bool
+pairFullyContains (a, b) = a `contains` b || b `contains` a
+
+fullyContained :: T.Text -> Either String Int
+fullyContained t =
+  let pairs = parsePairs t
+  in length <$> filter pairFullyContains <$> pairs
+
+anyOverlap :: T.Text -> Either String Int
+anyOverlap t =
+  let pairs = parsePairs t
+  in length <$> filter (uncurry overlaps) <$> pairs
 
 answers :: IO ()
 answers = do
   input <- readInput
   putStrLn "Part 1:"
+  print (fullyContained input)
   putStrLn "Part 2:"
+  print (anyOverlap input)
--- test/Aoc/Day04Spec.hs
+++ test/Aoc/Day04Spec.hs
@@ -11,6 +11,14 @@ import qualified Data.Text as T
 spec :: Spec
 spec = do
   describe "example data" $ do
-    let exampleData = ""
+    let exampleData = "2-4,6-8\n2-3,4-5\n5-7,7-9\n2-8,3-7\n6-6,4-6\n2-6,4-8"
+    it "part 1: 2-4,6-8 not fully contained" $
+      fullyContained "2-4,6-8" === pure 0
+    it "part 1: 2-8,3-7 is fully contained" $
+      fullyContained "2-8,3-7" === pure 1
+    it "part 1: [(2-4,6-8), (2-8,3-7)] has one fully contained" $
+      fullyContained "2-4,6-8\n2-8,3-7" === pure 1
     it "part 1: example" $
-      1 === 1
+      fullyContained exampleData === pure 2
+    it "part 2: example" $
+      anyOverlap exampleData === pure 4
