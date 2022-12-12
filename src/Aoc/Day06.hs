e6d96cdaa37faacd67e658fbbb83c79cde30636d
David Tchepak <dave@davesquared.net>
2022-12-12 16:49:14 +1100

    Day 6 parts 1 and 2

--- src/Aoc/Day06.hs
+++ src/Aoc/Day06.hs
@@ -1,20 +1,36 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Aoc.Day06 where
 
+import Data.List (find, nub)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
-import Data.Void
-import Text.Megaparsec hiding (State)
-import Text.Megaparsec.Char
-import qualified Text.Megaparsec.Char.Lexer as L
+
+type PacketSize = Int
 
 readInput :: IO T.Text
 readInput = T.readFile "data/day06.txt"
+
+packets :: PacketSize -> T.Text -> [(Int, T.Text)]
+packets n = zip [0..] . map (T.take n) . T.tails
+
+isDistinct :: T.Text -> Bool
+isDistinct x = (== T.length x) . length . nub . T.unpack $ x
+
+findDistinct :: PacketSize -> T.Text -> Maybe Int
+findDistinct n =
+  fmap ((+n) . fst) . find (isDistinct . snd) . packets n
+
+marker :: T.Text -> Maybe Int
+marker = findDistinct 4
+
+message :: T.Text -> Maybe Int
+message = findDistinct 14
+
 
 answers :: IO ()
 answers = do
   input <- readInput
   putStrLn "Part 1:"
+  print $ marker input
   putStrLn "Part 2:"
-
-type Parser = Parsec Void T.Text
+  print $ message input
--- test/Aoc/Day06Spec.hs
+++ test/Aoc/Day06Spec.hs
@@ -8,5 +8,13 @@ import Test.QuickCheck
 spec :: Spec
 spec = do
   describe "example data" $ do
-    it "part 1: first move example" $
-      1 === 1
+    it "part 1: example 1" $
+      marker "mjqjpqmgbljsphdztnvjfqwrcgsmlb" === Just 7
+    it "part 1: example 2" $
+      marker "bvwbjplbgvbhsrlpgdmjqwftvncz" === Just 5
+    it "part 1: example 3" $
+      marker "nppdvjthqldpwncqszvftbrmjlhg" === Just 6
+    it "part 1: example 4" $
+      marker "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg" === Just 10
+    it "part 1: example 5" $
+      marker "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw" === Just 11
