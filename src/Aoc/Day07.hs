430ad3455ebabfb75ca478162138c0e2baf2d7df
David Tchepak <dave@davesquared.net>
2022-12-14 10:10:58 +1100

    Starting Day 7 part 1

--- src/Aoc/Day07.hs
+++ src/Aoc/Day07.hs
@@ -1,12 +1,78 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Aoc.Day07 where
 
+import Data.List (foldl')
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Foldable (fold)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
-import Data.Void
-import Text.Megaparsec hiding (State)
-import Text.Megaparsec.Char
-import qualified Text.Megaparsec.Char.Lexer as L
+import Text.Read (readEither)
+
+-- | Size of files in a directory (excluding sub-directories)
+type DirectFileSize = Int
+
+data Path = Path [T.Text]
+  deriving (Show, Eq, Ord)
+
+toPath :: T.Text -> Path
+toPath = Path . reverse . filter (not . T.null) . T.splitOn "/"
+
+pathIn :: Path -> T.Text -> Path
+pathIn (Path xs) = Path . (: xs)
+
+pathUp :: Path -> Path
+pathUp (Path xs) = Path (drop 1 xs)
+
+pathDepth :: Path -> Int
+pathDepth (Path xs) = length xs
+ 
+data DirectoryContents = DirectoryContents { size :: DirectFileSize }
+  deriving (Show, Eq)
+
+instance Semigroup DirectoryContents where
+  a <> b = DirectoryContents (size a + size b)
+
+instance Monoid DirectoryContents where
+  mempty = DirectoryContents 0
+
+type FileSystem = Map Path DirectoryContents
+
+data FsBrowseState = FsBrowseState { path :: Path, fs :: FileSystem }
+  deriving (Show, Eq)
+
+newBrowse :: FsBrowseState
+newBrowse = FsBrowseState (Path []) Map.empty
+
+addFile :: Path -> DirectoryContents -> FileSystem -> FileSystem
+addFile path d =
+  Map.alter (pure . (d <>) . fold) path
+
+cd :: T.Text -> FsBrowseState -> FsBrowseState
+cd dir s =
+  if dir == "/" then s { path = Path [] }
+  else if dir == ".." then s { path = pathUp (path s) }
+  else s { path = pathIn (path s) dir }
+
+lsEntry :: DirectoryContents -> FsBrowseState -> FsBrowseState
+lsEntry d state = state { fs = addFile (path state) d (fs state) }
+
+readContents :: T.Text -> DirectoryContents
+readContents t = either (\e -> error $ e ++ ' ' : show t) DirectoryContents . readEither . T.unpack $ t
+
+parseLine :: [T.Text] -> FsBrowseState -> FsBrowseState
+parseLine ["$", "cd", x] = cd x
+parseLine ["$", "ls"] = id
+parseLine ["dir", _] = id
+parseLine [fileSize, _] = lsEntry . readContents $ fileSize
+parseLine x = error $ "unrecognised line: " ++ show x
+
+list :: FileSystem -> T.Text -> Maybe DirectoryContents
+list fs t = Map.lookup (toPath t) fs
+
+parseSession :: T.Text -> FileSystem
+parseSession =
+  fs . foldl' (flip (parseLine . T.words)) newBrowse .  T.lines
 
 readInput :: IO T.Text
 readInput = T.readFile "data/day07.txt"
@@ -16,5 +82,3 @@ answers = do
   input <- readInput
   putStrLn "Part 1:"
   putStrLn "Part 2:"
-
-type Parser = Parsec Void T.Text
--- test/Aoc/Day07Spec.hs
+++ test/Aoc/Day07Spec.hs
@@ -1,12 +1,61 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, QuasiQuotes #-}
 module Aoc.Day07Spec (spec) where
 
 import Aoc.Day07
+import qualified Data.Text as T
+import qualified Data.Map as Map
+import Text.RawString.QQ
 import Test.Hspec
 import Test.QuickCheck
 
+exampleData :: T.Text
+exampleData = [r|$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k|]
+
 spec :: Spec
 spec = do
-  describe "example data" $ do
+  describe "path" $ do
+    it "path in" $
+      pathIn (toPath "/a/b") "c" === toPath "/a/b/c"
+    it "path up" $
+      pathUp (toPath "/a/b") === toPath "/a"
+  describe "cd" $ do
+    it "cd a" $
+      path (cd "a" newBrowse) === toPath "/a"
+  describe "session" $ do
+    it "sample session" $
+      let fs = parseSession [r|$ cd /
+$ cd abc
+$ ls
+1 a
+2 b
+$ cd def
+$ ls
+dir ghi
+10 c|]
+      in fmap (fmap size . list fs) ["/abc", "/abc/def"] === [Just 3, Just 10]
+  describe "example data" $ do 
     it "part 1: example 1" $
-      1 === 1
+      let fs = parseSession exampleData
+      in fmap (fmap size . list fs) ["/d", "/a/e"] === [Just 24933642, Just 584]
